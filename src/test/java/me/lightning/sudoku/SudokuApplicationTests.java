5c803eeeb767d7cd8d105b18b61fc726f98bb9c7
dake421e <daniel.kessler@mailbox.tu-dresden.de>
2024-05-27 09:35:29 +0200

    Sudoku init

---
+++ src/test/java/me/lightning/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lightning.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
